738b8723f9518e24d7c6332e25c1e57995290c66
Dave Steinich <drsteini@usgs.gov>
2014-02-06 18:01:41 +0000

    WQP-265 Improve WQP database query responses

--- create_nad_objects_eros.sql
+++ create_nad_objects_eros.sql
@@ -752,7 +752,7 @@ create or replace package body create_nad_objects
           end */ cast(NULL as varchar2(32)) as WQX_STATION_TYPE,
           mdsys.sdo_geometry(2001,8265,mdsys.sdo_point_type(round(dec_long_va, 7),round(dec_lat_va, 7), null), null, null) as GEOM,
           fips.state_fips,
-          site_tp.primary_site_type
+          cast(site_tp.primary_site_type as varchar2(30)) primary_site_type
       from
           nwis_ws_star.sitefile sitefile,
          (select cast(''USGS-'' || state_postal_cd as varchar2(7)) as organization_id,
