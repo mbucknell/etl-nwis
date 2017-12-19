2016923fdb173c951761ee67a50993bbfe5d121d
Steinich, Dave R. <drsteini@usgs.gov>
2017-12-19 09:34:27 -0600

    WQP-1174 - calculate Huc12 for NWIS

--- transformStation.sql
+++ transformStation.sql
@@ -6,9 +6,59 @@ whenever sqlerror exit failure rollback;
 whenever oserror exit failure rollback;
 select 'transform station start time: ' || systimestamp from dual;
 
+merge into nwis_station_local o 
+      using (select /*+ parallel(4) */ 
+                    sitefile.site_id station_id,
+                    sitefile.agency_cd || '-' || sitefile.site_no site_id,
+                    round(sitefile.dec_lat_va , 7) latitude,
+                    round(sitefile.dec_long_va, 7) longitude,
+                    sitefile.huc_cd huc,
+                    case when sitefile.dec_long_va is not null and sitefile.dec_lat_va is not null
+                      then mdsys.sdo_geometry(2001,
+                                              4269,
+                                              mdsys.sdo_point_type(round(sitefile.dec_long_va, 7),
+                                                                   round(sitefile.dec_lat_va, 7),
+                                                                   null),
+                                              null, null)
+                    else
+                      null
+                    end geom
+               from nwis_ws_star.sitefile
+            ) n
+  on (o.station_id = n.station_id)
+when matched then update
+                     set o.site_id = n.site_id,
+                         o.latitude = n.latitude,
+                         o.longitude = n.longitude,
+                         o.huc = n.huc,
+                         o.calculated_huc_12 = null,
+                         o.geom = n.geom
+                   where lnnvl(o.latitude = n.latitude) or
+                         lnnvl(o.longitude = n.longitude) or
+                         lnnvl(o.huc = n.huc)
+when not matched then insert (station_id, site_id, latitude, longitude, huc, geom)
+                      values (n.station_id, n.site_id, n.latitude, n.longitude, n.huc, n.geom);
+commit;
+select 'Merge into nwis_station_local complete: ' || systimestamp from dual;
+
+prompt calculating huc
+merge into nwis_station_local o 
+      using (select /*+ parallel(4) */ 
+                    station_id,
+                    huc12
+               from huc12nometa,
+                    nwis_station_local
+              where nwis_station_local.geom is not null and
+                    calculated_huc_12 is null and
+                    sdo_contains(huc12nometa.geometry, nwis_station_local.geom) = 'TRUE') n
+   on (o.station_id = n.station_id)
+when matched then update set calculated_huc_12 = huc12;
+commit;
+select 'Calculating HUC complete: ' || systimestamp from dual;
+
 prompt dropping nwis station indexes
 exec etl_helper_station.drop_indexes('nwis');
-        
+
 prompt populating nwis station
 truncate table station_swap_nwis;
 
@@ -27,9 +77,9 @@ select /*+ parallel(4) */
        sitefile.agency_cd || '-' || sitefile.site_no site_id,
        ndcbh.organization_id organization,
        site_tp.primary_site_type site_type,
-       sitefile.huc_cd huc,
+       nvl(nwis_station_local.calculated_huc_12, sitefile.huc_cd) huc,
        sitefile.country_cd || ':' || sitefile.state_cd || ':' || sitefile.county_cd governmental_unit_code,
-       mdsys.sdo_geometry(2001,4269,mdsys.sdo_point_type(round(sitefile.dec_long_va, 7),round(sitefile.dec_lat_va, 7), null), null, null) geom,
+       nwis_station_local.geom,
        trim(sitefile.station_nm) station_name,
        ndcbh.organization_name,
        site_tp.station_type_name,
@@ -59,6 +109,8 @@ select /*+ parallel(4) */
        to_number(case when sitefile.hole_depth_va in ('.', '-') then '0' else sitefile.hole_depth_va end) hole_depth_value,
        nvl2(sitefile.hole_depth_va, 'ft', null) hole_depth_unit
   from nwis_ws_star.sitefile
+       join nwis_station_local
+         on sitefile.site_id = nwis_station_local.station_id
        join (select cast('USGS-' || state_postal_cd as varchar2(7)) organization_id,
                     'USGS ' || state_name || ' Water Science Center' organization_name,
                     host_name,
