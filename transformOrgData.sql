ba0b62306e88d2bd355cad2740dfd456c6f21220
Pattermann <dpattermann@usgs.gov>
2018-07-17 11:53:56 -0500

    Fix a bunch of org vs org_data typos

--- transformOrgData.sql
+++ transformOrgData.sql
@@ -4,16 +4,16 @@ set timing on;
 set serveroutput on;
 whenever sqlerror exit failure rollback;
 whenever oserror exit failure rollback;
-select 'transform org start time: ' || systimestamp from dual;
+select 'transform org_data start time: ' || systimestamp from dual;
 
-prompt building org_swap_nwis 
+prompt building org_data_swap_nwis 
 
-prompt dropping nwis org indexes
-exec etl_helper_org.drop_indexes('org');
+prompt dropping nwis org_data indexes
+exec etl_helper_org_data.drop_indexes('nwis');
 
 set define off;
 prompt populating org_data_swap_nwis
-truncate table org_swap_nwis;
+truncate table org_data_swap_nwis;
 insert /*+ append parallel(4) */
   into org_data_swap_nwis (data_source_id, data_source,organization, organization_name)
 select DISTINCT /* parallel(4) */
@@ -22,5 +22,3 @@ select DISTINCT /* parallel(4) */
 	   cast('USGS-' || state_postal_cd as varchar2(7)) organization,
        'USGS ' || state_name || ' Water Science Center' organization_name
   from nwis_ws_star.nwis_district_cds_by_host
-			
-			
