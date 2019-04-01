af0c1b2d0c0a518817486db3656c05452aaf503d
Mary Bucknell <mbucknell@usgs.gov>
2019-04-01 10:11:23 -0500

    Created a NwisBaseFlowIT class to be used for flow integration tests. Fixed TransformrgDataIT and pom so that the test runs and executes without failures

--- src/test/java/gov/acwi/wqp/etl/orgData/TransformOrgDataIT.java
+++ src/test/java/gov/acwi/wqp/etl/orgData/TransformOrgDataIT.java
@@ -3,37 +3,16 @@ package gov.acwi.wqp.etl.orgData;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import java.nio.charset.Charset;
-import java.sql.SQLException;
-
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.StepScopeTestExecutionListener;
 import org.springframework.batch.test.context.SpringBatchTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.EncodedResource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.init.ScriptException;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -45,8 +24,8 @@ import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 
-import gov.acwi.wqp.etl.BaseFlowIT;
 import gov.acwi.wqp.etl.DBTestConfig;
+import gov.acwi.wqp.etl.NwisBaseFlowIT;
 import gov.acwi.wqp.etl.NwisDBTestConfig;
 
 
@@ -61,38 +40,7 @@ import gov.acwi.wqp.etl.NwisDBTestConfig;
 @Import({DBTestConfig.class, NwisDBTestConfig.class})
 @DbUnitConfiguration(databaseConnection={"wqp","pg", "nwis"})
 @DirtiesContext
-public class TransformOrgDataIT {
-
-	@Autowired
-	protected JdbcTemplate jdbcTemplate;
-	@Autowired
-	protected JobBuilderFactory jobBuilderFactory;
-	@Autowired
-	protected JobLauncherTestUtils jobLauncherTestUtils;
-	@Autowired
-	protected DataSource dataSource;
-	@Value("classpath:db/test_db.sql")
-	protected Resource resource;
-
-	protected JobParameters testJobParameters;
-
-	protected Job testJob;
-
-	@PostConstruct
-	public void beforeClass() throws ScriptException, SQLException {
-		EncodedResource encodedResource = new EncodedResource(resource, Charset.forName("UTF-8"));
-		ScriptUtils.executeSqlScript(dataSource.getConnection(), encodedResource);
-	}
-
-	@Before
-	public void baseSetup() {
-		testJobParameters= new JobParametersBuilder()
-				.addJobParameters(jobLauncherTestUtils.getUniqueJobParameters())
-				.addString("wqpDataSourceId", "2", true)
-				.addString("wqpDataSource", "nwis", true)
-				.addString("schemaName", "wqp", false)
-				.toJobParameters();
-	}
+public class TransformOrgDataIT extends NwisBaseFlowIT{
 
 	@Test
 	@DatabaseSetup(value="classpath:/testResult/wqp/orgData/empty.xml")
