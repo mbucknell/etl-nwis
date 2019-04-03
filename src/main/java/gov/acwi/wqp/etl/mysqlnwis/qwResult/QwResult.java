0da90729cc2014d928f04fcace7e49bb45c687e6
Mary Bucknell <mbucknell@usgs.gov>
2019-04-03 13:08:57 -0500

    The rest of the QwResult transformation

---
+++ src/main/java/gov/acwi/wqp/etl/mysqlnwis/qwResult/QwResult.java
@@ -0,0 +1,190 @@
+package gov.acwi.wqp.etl.mysqlnwis.qwResult;
+
+import java.util.Date;
+
+public class QwResult {
+	
+	private int sampleId;
+	private int siteId;
+	private String recordNo;
+	private boolean resultWebCd;
+	private String parameterCd;
+	private String methCd;
+	private String resultVa;
+	private String resultUnrndVa;
+	private String resultRd;
+	private String rptLevVa;
+	private String rptLevCd;
+	private String labStdVa;
+	private String remarkCd;
+	private String valQualTx;
+	private String nullValQualCd;
+	private String qaCd;
+	private String dqiCd;
+	private String anlEntCd;
+	private String anlSetNo;
+	private String anlDt;
+	private String prepSetNo;
+	private String prepDt;
+	private String resultFieldCmTx;
+	private String resultLabCmTx;
+	private Date resultMd;
+	private Date qwResultMd;
+	
+	public int getSampleId() {
+		return sampleId;
+	}
+	public void setSampleId(int sampleId) {
+		this.sampleId = sampleId;
+	}
+	public int getSiteId() {
+		return siteId;
+	}
+	public void setSiteId(int siteId) {
+		this.siteId = siteId;
+	}
+	public String getRecordNo() {
+		return recordNo;
+	}
+	public void setRecordNo(String recordNo) {
+		this.recordNo = recordNo;
+	}
+	public boolean isResultWebCd() {
+		return resultWebCd;
+	}
+	public void setResultWebCd(boolean resultWebCd) {
+		this.resultWebCd = resultWebCd;
+	}
+	public String getParameterCd() {
+		return parameterCd;
+	}
+	public void setParameterCd(String parameterCd) {
+		this.parameterCd = parameterCd;
+	}
+	public String getMethCd() {
+		return methCd;
+	}
+	public void setMethCd(String methCd) {
+		this.methCd = methCd;
+	}
+	public String getResultVa() {
+		return resultVa;
+	}
+	public void setResultVa(String resultVa) {
+		this.resultVa = resultVa;
+	}
+	public String getResultUnrndVa() {
+		return resultUnrndVa;
+	}
+	public void setResultUnrndVa(String resultUnrndVa) {
+		this.resultUnrndVa = resultUnrndVa;
+	}
+	public String getResultRd() {
+		return resultRd;
+	}
+	public void setResultRd(String resultRd) {
+		this.resultRd = resultRd;
+	}
+	public String getRptLevVa() {
+		return rptLevVa;
+	}
+	public void setRptLevVa(String rptLevVa) {
+		this.rptLevVa = rptLevVa;
+	}
+	public String getRptLevCd() {
+		return rptLevCd;
+	}
+	public void setRptLevCd(String rptLevCd) {
+		this.rptLevCd = rptLevCd;
+	}
+	public String getLabStdVa() {
+		return labStdVa;
+	}
+	public void setLabStdVa(String labStdVa) {
+		this.labStdVa = labStdVa;
+	}
+	public String getRemarkCd() {
+		return remarkCd;
+	}
+	public void setRemarkCd(String remarkCd) {
+		this.remarkCd = remarkCd;
+	}
+	public String getValQualTx() {
+		return valQualTx;
+	}
+	public void setValQualTx(String valQualTx) {
+		this.valQualTx = valQualTx;
+	}
+	public String getNullValQualCd() {
+		return nullValQualCd;
+	}
+	public void setNullValQualCd(String nullValQualCd) {
+		this.nullValQualCd = nullValQualCd;
+	}
+	public String getQaCd() {
+		return qaCd;
+	}
+	public void setQaCd(String qaCd) {
+		this.qaCd = qaCd;
+	}
+	public String getDqiCd() {
+		return dqiCd;
+	}
+	public void setDqiCd(String dqiCd) {
+		this.dqiCd = dqiCd;
+	}
+	public String getAnlEntCd() {
+		return anlEntCd;
+	}
+	public void setAnlEntCd(String anlEntCd) {
+		this.anlEntCd = anlEntCd;
+	}
+	public String getAnlSetNo() {
+		return anlSetNo;
+	}
+	public void setAnlSetNo(String anlSetNo) {
+		this.anlSetNo = anlSetNo;
+	}
+	public String getAnlDt() {
+		return anlDt;
+	}
+	public void setAnlDt(String anlDt) {
+		this.anlDt = anlDt;
+	}
+	public String getPrepSetNo() {
+		return prepSetNo;
+	}
+	public void setPrepSetNo(String prepSetNo) {
+		this.prepSetNo = prepSetNo;
+	}
+	public String getPrepDt() {
+		return prepDt;
+	}
+	public void setPrepDt(String prepDt) {
+		this.prepDt = prepDt;
+	}
+	public String getResultFieldCmTx() {
+		return resultFieldCmTx;
+	}
+	public void setResultFieldCmTx(String resultFieldCmTx) {
+		this.resultFieldCmTx = resultFieldCmTx;
+	}
+	public String getResultLabCmTx() {
+		return resultLabCmTx;
+	}
+	public void setResultLabCmTx(String resultLabCmTx) {
+		this.resultLabCmTx = resultLabCmTx;
+	}
+	public Date getResultMd() {
+		return resultMd;
+	}
+	public void setResultMd(Date resultMd) {
+		this.resultMd = resultMd;
+	}
+	public Date getQwResultMd() {
+		return qwResultMd;
+	}
+	public void setQwResultMd(Date qwResultMd) {
+		this.qwResultMd = qwResultMd;
+	}
+}
---
+++ src/main/java/gov/acwi/wqp/etl/mysqlnwis/qwResult/TransformQwResult.java
@@ -0,0 +1,70 @@
+package gov.acwi.wqp.etl.mysqlnwis.qwResult;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.support.PassThroughItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import gov.acwi.wqp.etl.mysqlnwis.qwSample.QwSample;
+import gov.acwi.wqp.etl.mysqlnwis.qwSample.QwSampleRowMapper;
+
+@Component
+public class TransformQwResult {
+
+	@Autowired
+	@Qualifier("mysqlnwisDataSource")
+	private DataSource mysqlnwisDataSource;
+
+	@Autowired
+	@Qualifier("nwisDataSource")
+	private DataSource nwisDataSource;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public JdbcCursorItemReader<QwResult> qwResultReader() {
+		return new JdbcCursorItemReaderBuilder<QwResult>()
+				.dataSource(mysqlnwisDataSource)
+				.name("mysqlQwResultReader")
+				.sql("select * from QW_RESULT limit 2000") //TODO remove limit
+				.rowMapper(new QwResultRowMapper())
+				.build();
+	}
+	
+	@Bean
+	public PassThroughItemProcessor<QwResult> qwResultProcessor() {
+		return new PassThroughItemProcessor<QwResult>();
+	}
+	
+	@Bean
+	public JdbcBatchItemWriter<QwResult> qwResultWriter() {
+		return new JdbcBatchItemWriterBuilder<QwResult>()
+				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+				.sql("insert into qw_result ("
+						+ "sample_id, site_id, record_no, result_web_cd, parameter_cd, "
+						+ "meth_cd, result_va, result_unrnd_va, result_rd, rpt_lev_va, "
+						+ "rpt_lev_cd, lab_std_va, remark_cd, val_qual_tx, null_val_qual_cd, "
+						+ "qa_cd, dqi_cd, anl_ent_cd, anl_set_no, anl_dt, prep_set_no, "
+						+ "prep_dt, result_field_cm_tx, result_lab_cm_tx, result_md, "
+						+ "qw_result_md) VALUES (" 
+						+ ":sampleId, :siteId, :recordNo, :resultWebCd, :parameterCd, "
+						+ ":methCd, :resultVa, :resultUnrndVa, :resultRd, :rptLevVa, "
+						+ ":rptLevCd, :labStdVa, :remarkCd, :valQualTx, :nullValQualCd, "
+						+ ":qaCd, :dqiCd, :anlEntCd, :anlSetNo, :anlDt, :prepSetNo, "
+						+ ":prepDt, :resultFieldCmTx, :resultLabCmTx, :resultMd, "
+						+ ":qwResultMd)")
+				.dataSource(nwisDataSource)
+				.build();
+	}
+						
+}
