ef56cf3ab99a3951aff0ade7fc8d2331901505a4
Mary Bucknell <mbucknell@usgs.gov>
2019-04-26 10:32:23 -0500

    Fixed autowiring issues in the tests. Needed to declare the jdbcTemplateWqp as primary in NwisDBTestConfig.java so that the correct template is used for the tests that need the wqp source.

--- src/test/java/gov/acwi/wqp/etl/NwisDBTestConfig.java
+++ src/test/java/gov/acwi/wqp/etl/NwisDBTestConfig.java
@@ -2,22 +2,39 @@ package gov.acwi.wqp.etl;
 
 import javax.sql.DataSource;
 
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.github.springtestdbunit.bean.DatabaseConfigBean;
+import com.github.springtestdbunit.bean.DatabaseDataSourceConnectionFactoryBean;
 
 @TestConfiguration
 public class NwisDBTestConfig {
 
 	@Autowired
+	private DatabaseConfigBean dbUnitDatabaseConfig;
+
+	@Autowired
+	@Qualifier("dataSourceWqp")
+	private DataSource dataSourceWqp;
+
+	@Autowired
 	@Qualifier(Application.DATASOURCE_NWIS_QUALIFIER)
 	private DataSource dataSourceNwis;
+
+	@Bean
+	@Primary
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSourceWqp);
+	}
 
 	@Bean
 	public JdbcTemplate jdbcTemplateNwis() {
