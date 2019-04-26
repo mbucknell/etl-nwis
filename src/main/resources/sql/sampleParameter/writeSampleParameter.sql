6a795075dc104305e7e520ceae4ca82b61e178c4
Mary Bucknell <mbucknell@usgs.gov>
2019-04-26 14:32:51 -0500

    Tweaks to sql as requested.

--- src/main/resources/sql/sampleParameter/writeSampleParameter.sql
+++ src/main/resources/sql/sampleParameter/writeSampleParameter.sql
@@ -16,41 +16,29 @@ select distinct qw_result.sample_id,
     v82398_fxd_tx,
     v84164_fxd_tx
 from qw_result
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '71999'
-    group by sample_id) v71999
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '71999') v71999
     on qw_result.sample_id = v71999.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '50280'
-    group by sample_id) v50280
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '50280') v50280
     on  qw_result.sample_id = v50280.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '72015'
-    group by sample_id) v72015
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '72015') v72015
     on  qw_result.sample_id = v72015.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '82047'
-    group by sample_id) v82047
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '82047') v82047
     on  qw_result.sample_id = v82047.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '72016'
-    group by sample_id) v72016
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '72016') v72016
     on  qw_result.sample_id = v72016.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '82048'
-    group by sample_id) v82048
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '82048') v82048
     on  qw_result.sample_id = v82048.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '00003'
-    group by sample_id) v00003
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '00003') v00003
     on  qw_result.sample_id = v00003.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '00098'
-    group by sample_id) v00098
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '00098') v00098
     on  qw_result.sample_id = v00098.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '78890'
-    group by sample_id) v78890
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '78890') v78890
     on  qw_result.sample_id = v78890.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '78891'
-    group by sample_id) v78891
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '78891') v78891
     on  qw_result.sample_id = v78891.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '82398'
-    group by sample_id) v82398
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '82398') v82398
     on  qw_result.sample_id = v82398.sample_id
-left outer join (select sample_id, max(result_unrnd_va) v from qw_result where result_web_cd and parameter_cd = '84164'
-    group by sample_id) v84164
+left join (select sample_id, result_unrnd_va v from qw_result where result_web_cd and parameter_cd = '84164') v84164
     on  qw_result.sample_id = v84164.sample_id
 left join (select fxd_nm v71999_fxd_nm, fxd_va from fxd where parm_cd = '71999') fxd_71999
          on cast(v71999.v as numeric) = fxd_71999.fxd_va
