2ccc98c182c0e85916f1481d780449aff102a0fe
Mary Bucknell <mbucknell@usgs.gov>
2019-05-14 09:16:14 -0500

    Add the qwSampleFlow back in and comment out qwResultFlow and sitefileFlow

--- src/main/java/gov/acwi/wqp/etl/mysqlnwis/MySqlNwisExtract.java
+++ src/main/java/gov/acwi/wqp/etl/mysqlnwis/MySqlNwisExtract.java
@@ -26,10 +26,9 @@ public class MySqlNwisExtract {
 	@Bean
 	public Flow mySqlNwisExtractFlow() {
 		return new FlowBuilder<SimpleFlow>("mySqlNwisExtractFlow")
-				//.start(qwSampleFlow)
-				.start(qwResultFlow)
+				.start(qwSampleFlow)
 				//.next(qwResultFlow)
-				.next(sitefileFlow)
+				//.next(sitefileFlow)
 				.build();
 	}
 }
